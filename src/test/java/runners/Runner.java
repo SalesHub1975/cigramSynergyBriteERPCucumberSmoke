773f7fa1a238597977717b3c380dc60f131c04a2
Your Name <you@example.com>
2019-07-30 02:14:40 -0500

    Added my code(Ramis )

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -5,13 +5,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-
         plugin = "html:target/cucumberReports",
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@helpButtonVerification",
+        tags = "@repairsMainPage",
         dryRun = false
 
 )
