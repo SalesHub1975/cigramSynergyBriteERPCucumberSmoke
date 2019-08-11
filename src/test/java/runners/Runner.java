9560d79e3e3f89b191ad9560a15109001ef5c975
SalesHub1975 <bakitgulmira@gmail.com>
2019-08-10 19:57:34 -0500

    Gn apdate again too

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,11 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumberReports", "json:target/report.json"},
+        plugin = {"html:target/cucumberReports", "json:target/report.json", "pretty"},
         features = "src/test/resources/features",
         glue = "steps",
          tags = "@searchBoxVerification",
-        dryRun = false
+        dryRun = false,
+        monochrome=true
 )
 public class Runner {
 
